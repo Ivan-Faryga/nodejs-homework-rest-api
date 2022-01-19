066bbecee5daca6d17779971b10c42b947fdcfc1
Ivan Faryga <ifaryga@gmail.com>
2022-01-19 02:07:50 +0200

    most of the tasks done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { User } = require("../../model");
-const router = express.Router();
-const { authenticate } = require("../../middlwares");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 
-module.exports = router;
+const { authenticate, upload } = require("../../middlwares");
+
+const router = express.Router();
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 router.get("/current", authenticate, async (req, res, next) => {
   const { name, email } = req.user;
@@ -21,3 +26,25 @@ router.get("/signout", authenticate, async (req, res) => {
   await User.findByIdAndUpdate(_id, { token: null });
   res.status(204).send();
 });
+
+// updating avatar
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  async (req, res) => {
+    const { path: tempUpload, filename } = req.file;
+    const [extension] = filename.split(".").reverse();
+    const newFileName = `${req.user._id}.${extension}`;
+    const fileUpload = path.join(avatarsDir, newFileName);
+    const modifiedFile = await Jimp.read(tempUpload);
+    modifiedFile.resize(250, 250).write(tempUpload);
+    await fs.rename(tempUpload, fileUpload);
+    const avatarURL = path.join("avatars", newFileName);
+    console.log(avatarURL);
+    await User.findByIdAndUpdate(req.user._id, { avatarURL }, { new: true });
+    res.json({ avatarURL });
+  }
+);
+
+module.exports = router;
